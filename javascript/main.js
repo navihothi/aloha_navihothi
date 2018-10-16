7f69fd482578370e331f18d7189a418fb7eb704e
Navi Hothi <hothi.navi@gmail.com>
2018-10-16 09:31:52 -0400

    Added some extra styling to the mobile RWD

--- javascript/main.js
+++ javascript/main.js
@@ -16,14 +16,37 @@ $(document).ready(function(){
     });
 
     //adding items to cart
+    $('.addtocart').on('click', () => {
+        console.log("Updating the Cart!");
+    
+        var cartUpdate = $('#cartUpdate');
 
+        var cartCount = parseInt(cartUpdate.text());
 
-    //subscribe button
-    $('#subscribe').on('click', function() {
-        alert('Thanks for subscribing!'); 
-    });
+        if(cartCount == 0) cartUpdate.css("display", "flex");
+        cartUpdate.text(++cartCount);
 
+      });
 
     
+    //validating email address
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }
+    
+    function validate() {
+        var email = $("#email").val();
+
+        if (validateEmail(email)) {
+            alert("Thanks for subscribing!");
+        } else {
+            alert("Please enter a valid email!");
+        } 
+        return false;
+        }
+
+        $("#subscribe").bind("click", validate);
+
 });
 
